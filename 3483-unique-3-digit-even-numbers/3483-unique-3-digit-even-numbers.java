e4a28ce5cf2f76dad35081eed7bdcd21d008ef39
Vansh <tyagig9520@gmail.com>
2026-09-11 14:54:02 +0530

    Time: 9 ms (26.4%), Space: 47 MB (11.87%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,27 +1,36 @@
 class Solution {
     public int totalNumbers(int[] digits) {
         
-        int n = digits.length;
-        HashSet<Integer> seen = new HashSet<>();
-
-        for(int i = 0; i < n; i++){
-
-            if(digits[i] == 0) continue;
-            for(int j = 0; j < n; j++){
-
-                if(i == j) continue;
-                for(int k = 0; k < n; k++){
-
-                    if(k == i || k == j) continue;
-                    if(digits[k] % 2 != 0) continue;
-
-                    int digit = (digits[i] * 100) + (digits[j] * 10) + digits[k];
-                    System.out.println(digit);
-
-                    seen.add(digit);
-                }
+        int result = 0;
+        int[] freq = new int[10];
+        for(int digit : digits){
+            freq[digit]++;
+        }
+        for(int i = 100; i <= 998; i += 2){
+            if(canForm(freq, i)){
+                result++;
             }
         }
-        return seen.size();
+
+        return result;
+    }
+
+    private boolean canForm(int[] freq, int digit){
+        int h = digit / 100;
+        int t = (digit / 10) % 10;
+        int o = digit % 10;
+
+        int[] newFreq = new int[10];
+        newFreq[h]++;
+        newFreq[t]++;
+        newFreq[o]++;
+
+        for(int i = 0; i < 10; i++){
+            if(newFreq[i] > freq[i]){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
